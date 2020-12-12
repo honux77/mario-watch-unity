6d1edb5b76448f5e39a59c65972ca1a3bfacfa3d
Hoyoung Jung <honux77@gmail.com>
2020-12-12 11:49:33 +0900

    Add title and mario

---
+++ Assets/Mario.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mario : MonoBehaviour
+{
+    public float speed = 3.0f;
+    bool seeRight = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = transform.position;
+        float dx = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        if (dx > 0 && !seeRight || dx < 0 && seeRight) {
+            flip();
+        }
+        pos.x +=  dx;
+        transform.position = pos;
+    }
+
+    void flip() {
+        seeRight = !seeRight;
+        Vector2 lc = transform.localScale;
+        lc.x *= -1;
+        transform.localScale = lc;        
+    }
+}
